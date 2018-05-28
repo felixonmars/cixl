1169514513cedcb664b93b8cba992b3456ef0a49
Sifoo <basic.gongfu@gmail.com>
2018-05-28 21:21:21 +0200

    added coro bench

---
+++ perf/bench8.lua
@@ -0,0 +1,17 @@
+n = 10000
+
+function f()
+  for i=1,n do
+    coroutine.yield(i)
+  end
+end
+
+t1 = os.clock()
+c = coroutine.create(f)
+
+for i=1,n do
+  _, j = coroutine.resume(c) 
+  assert(i == j)
+end
+
+print((os.clock() - t1) * 1000)
